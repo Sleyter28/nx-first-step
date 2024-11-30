2bd6099c3f144f53f51e9777ac934a0db7503b9f
Sleyter28 <sley9468@gmail.com>
2024-11-29 21:17:06 -0600

    Initial commit

---
+++ app/(general)/about/page.tsx
@@ -0,0 +1,13 @@
+
+export const metadata = {
+ title: 'About our company',
+ description: 'SEO Description',
+ keywords: ['SEO', 'Next.js', 'TypeScript'],
+};
+export default function AboutPage() {
+    return (
+        <>
+            <span className="text-7xl">About page</span>
+        </>
+    )
+}
